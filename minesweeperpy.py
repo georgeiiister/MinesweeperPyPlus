650c87685ac3b25279724350444cf3904228258e
georgeiiister <georgeiiister@gmail.com>
2024-08-07 23:15:49 +0300

    change class MButton

--- minesweeperpy.py
+++ minesweeperpy.py
@@ -3,13 +3,14 @@ import random
 import tkinter.messagebox as mb
 import logging
 
-logging.basicConfig(filename='MinesweeperPy.log', level=logging.DEBUG,format='%(asctime)s - %(levelname)s - %(message)s')
-
+logging.basicConfig(filename='MinesweeperPy.log', level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 size_game_field = 'size_game_field'
 number_of_bombs_coordinates = 'number_of_bombs_coordinates'
 title_for_boom = 'title_for_boom'
 message_for_boom = 'message_for_boom'
+
 
 class MButton(tk.Button):
     def __init__(self, master, xy, width=None, height=None):
@@ -32,21 +33,50 @@ class MButton(tk.Button):
     def check_bomb(self):
         if self.bomb:
             mb.showinfo(title=get_tuning_item(title_for_boom), message=get_tuning_item(message_for_boom))
-            self.config(state='disabled', text=':(')
+            self.__set_disabled_boom()
         else:
-            self.config(state='disabled', text='ok')
+            self.__set_disabled_ok()
+
+    def __set_disabled_ok(self):
+        self.config(state='disabled', text='ok')
+
+    def __set_disabled_boom(self):
+        self.config(state='disabled', text=':(')
 
 
 class MFrame(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master=master)
+        self.__count_check_bomb = 0
+        self.__size_x = None
+        self.__size_y = None
+
+    def __set_count_check_bomb(self):
+        self.__count_check_bomb += 1
+
+    @property
+    def count_check_bomb(self):
+        return self.__count_check_bomb
+
+    @property
+    def size_x(self):
+        return self.__size_x
+
+    @property
+    def size_y(self):
+        return self.__size_y
 
     def create_widgets(self):
         size = get_tuning_item('size_game_field')
-        size_game_field_x = range(size[0])
-        size_game_field_y = range(size[1])
+        logging.debug(f'size game field {size}')
 
-        game_field = {(x, y): MButton(master=self, xy=(x, y)) for x in size_game_field_x for y in size_game_field_y}
+        self.__size_x = size[0]
+        self.__size_y = size[1]
+
+        range_x = range(self.__size_x)
+        range_y = range(self.__size_y)
+
+        game_field = {(x, y): MButton(master=self, xy=(x, y)) for x in range_x for y in range_y}
 
         bombs = random.sample(population=tuple(game_field.keys()), k=get_tuning_item(number_of_bombs_coordinates))
         logging.debug(f'bombs coordinate {bombs}')
@@ -83,7 +113,7 @@ def get_tuning_item(item_code) -> tuple | int | str:
     return get_tuning_dict()[item_code]
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     logging.debug(f'start debug')
     root = MWin()
     root.mainloop()
